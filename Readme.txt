dcdfcda1c7717c385aa5f5703892974446d321e6
ilesoviy <ilisovij861@gmail.com>
2023-08-07 18:41:14 +0900

    Commented

--- Readme.txt
+++ Readme.txt
@@ -1,5 +1,7 @@
 
+(Install libcpp-httplib-dev library)
 > sudo apt install libcpp-httplib-dev
-
+(Compile source)
 > g++ test.cpp -o test -l/usr/lib/x86_64-linux-gnu -lcpp-httplib
+(Test)
 > ./test
--- test.cpp
+++ test.cpp
@@ -3,15 +3,15 @@
 
 int main(void)
 {
-  httplib::Client cli("https://dog-api.kinduff.com");
+  httplib::Client cli("https://dog-api.kinduff.com");	// Connect to site
 
-  if (auto res = cli.Get("/api/facts?raw=true")) {
-    if (res->status == 200) {
-      std::cout << res->body << std::endl;
+  if (auto res = cli.Get("/api/facts?raw=true")) {	// send request
+    if (res->status == 200) {	// if success
+      std::cout << res->body << std::endl;	// print message
     }
     else
-      std::cout << "status: " << res->status << std::endl;
-  } else {
+      std::cout << "status: " << res->status << std::endl;	// else, print error status
+  } else { // if failed, print error string
     auto err = res.error();
     std::cout << "HTTP error: " << httplib::to_string(err) << std::endl;
   }
